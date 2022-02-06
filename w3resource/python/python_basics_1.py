ce24ca32ded2dbbf6ee4d744ecf6b628d42c63f8
kcall174 <kmacallaghan@gmail.com>
2022-02-06 02:27:40 -0500

    Create python_basics_1.py
    added solution code

---
+++ w3resource/python/python_basics_1.py
@@ -0,0 +1,21 @@
+#!python3
+#w3resource Python Basic: Exercise-1 with Solution
+#>>Directions<<: write program that prints one line at 
+#a time and in the format above.
+
+#the output we want
+'''
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+'''
+
+#storing the string in a variable to print easily.
+twinkle = "Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp Above the world so high, \n\t\tLike a diamon in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are"
+
+print(twinkle)
+
+    
