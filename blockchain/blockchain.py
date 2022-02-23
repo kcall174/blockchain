74a8171ffb80a04d78623872e402891ef12d2063
K.C <kmacallaghan@gmail.com>
2022-02-23 04:33:40 -0500

    Update blockchain.py

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,30 +1,30 @@
-#imports the Block class from block.py
 from block import Block
 
 class Blockchain:
   def __init__(self):
     self.chain = []
-    self.all_transactions = []
+    self.unconfirmed_transactions = []
     self.genesis_block()
 
   def genesis_block(self):
-    transactions = {}
+    transactions = []
     genesis_block = Block(transactions, "0")
+    genesis_block.generate_hash()
     self.chain.append(genesis_block)
-    return self.chain
 
-  # prints contents of blockchain
+  def add_block(self, transactions):
+    previous_hash = (self.chain[len(self.chain)-1]).hash
+    new_block = Block(transactions, previous_hash)
+    new_block.generate_hash()
+    proof = self.proof_of_work(new_block)
+    self.chain.append(new_block)
+    return proof, new_block
+
   def print_blocks(self):
     for i in range(len(self.chain)):
       current_block = self.chain[i]
       print("Block {} {}".format(i, current_block))
-      current_block.print_contents()    
-  
-  # add block to blockchain `chain`
-  def add_block(self, transactions):
-    previous_block_hash = self.chain[len(self.chain)-1].hash
-    new_block = Block(transactions, previous_block_hash)
-    self.chain.append(new_block)
+      current_block.print_contents()
 
   def validate_chain(self):
     for i in range(1, len(self.chain)):
