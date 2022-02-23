21432dc4fadf1e868c1878129ef6cc1b37e323d2
K.C <kmacallaghan@gmail.com>
2022-02-23 04:43:55 -0500

    Update transactions.py

--- blockchain/transactions.py
+++ blockchain/transactions.py
@@ -1,7 +1,6 @@
 # import datetime library
 from datetime import datetime
-# print current date and time
-#print(datetime.now())
+from blockchain import Blockchain
 
 class Block:
 # default constructor for block class
@@ -52,7 +51,21 @@ mempool.append(my_transaction)
 block_transactions = [my_transaction, transaction4, transaction6]
 print(block_transactions)
 
+#more transactions, using methods made. 
+block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
+block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
+block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
+fake_transactions = {"sender": "Bob", "receiver":"Cole, Alice", "amount":"25"}
 
+local_blockchain = Blockchain()
+local_blockchain.print_blocks()
+
+local_blockchain.add_block(block_one_transactions)
+local_blockchain.add_block(block_two_transactions)
+local_blockchain.add_block(block_three_transactions)
+local_blockchain.print_blocks()
+local_blockchain.chain[2].transactions = fake_transactions
+local_blockchain.validate_chain()
 
     
     
