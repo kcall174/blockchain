9573e0eecbd6c745173a965c1d0782a538ae3037
Ken <kmacallaghan@gmail.com>
2023-02-09 05:35:40 -0500

    Update sha256_hashing.py

--- blockchain/sha256_hashing.py
+++ blockchain/sha256_hashing.py
@@ -1,10 +1,8 @@
-# import sha256
+
 from hashlib import sha256
 
 # text to hash
 text = "I am excited to learn about blockchain"
-
-# print result
 print(text)
 
 hash_result = sha256(text.encode())
