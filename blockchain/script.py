0e0e7588dab603679fa841fc13e5ba7cb823081f
Ken <kmacallaghan@gmail.com>
2023-02-09 05:35:11 -0500

    Update script.py

--- blockchain/script.py
+++ blockchain/script.py
@@ -1,4 +1,4 @@
-# import sha256
+
 from hashlib import sha256
 # sets the amount of leading zeros that must be found in the hash produced by the nonce
 difficulty = 2
@@ -6,7 +6,6 @@ nonce = 0
 
 # creating the proof 
 proof = sha256((str(nonce)+str(new_transactions)).encode()).hexdigest()
-# printing proof
 print(proof)
   
 # finding a proof that has 2 leading zeros
